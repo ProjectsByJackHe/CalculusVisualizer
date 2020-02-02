cc7c4a45182b6a26f47746fb0c44a7376737b7cc
Jack He <jackhe2345@gmail.com>
2020-02-02 11:39:38 -0800

    got derivatives to work

--- src/js/Derivative.js
+++ src/js/Derivative.js
@@ -1,9 +1,9 @@
 import * as math from 'mathjs'
 
 
-// String Number -> List(Number, Number)
+// String Number -> List(List(Number, Number))
 // this function should return the derivative of a function at a specific point and f(x)
-function takeDx(func, point){
+function takeDx(func, start, end, increment){
     var derivativeFuncObj = math.derivative(func, "x")
 
     // string representation
@@ -14,7 +14,17 @@ function takeDx(func, point){
     var derivativeFuncEval = derivativeFuncObj.compile()
     // use .evaluate({x: point}) once the derivative function works.
 
-    return ["derivative", "F(x)"] // stub
+
+    var derivativesAndF = []
+
+    for (var point = start; point <= end; point += increment) {
+        let scope = {
+            x:point
+        }
+        derivativesAndF.push([derivativeFuncEval.evaluate(scope), math.evaluate(func, scope)])
+    }
+
+    return derivativesAndF // stub
 }
 
 export default takeDx
--- src/js/Render.js
+++ src/js/Render.js
@@ -2,12 +2,10 @@ import initializeGraph from './Main.js'
 import takeDx from './Derivative.js'
 
 class Render{
+    // String -> Image
+    // draws the graph onto the screen
     renderFunc(expression){
-        // console.log(expression)
-        var equation = "y = " + expression
-    
-        // console.log(equation)
-        initializeGraph.setExpression({ id: 'oneAndOnlyGraph', latex: equation, color: "#00FFFF"});
+        initializeGraph.setExpression({ id: 'oneAndOnlyGraph', latex: expression, color: "#00FFFF"});
     }
 
     // String -> String
@@ -16,7 +14,6 @@ class Render{
     // replace \sin x with sin(x), \sin\left(x\right) with sin(x)
     latexToExpression(latex) {
         var expr = ""
-
         // removes all instances of '{' and '}'
         // removes all instances of \left and \right
         for (var i = 0; i < latex.length; i++){
@@ -30,13 +27,42 @@ class Render{
                 }
             }
         }
-
         return expr
     }
 
     renderDerivative(latex, start, end) {
-      //    console.log(this.latexToExpression(latex))
-            takeDx(this.latexToExpression(latex), 0)
+
+            // INCREMENT is Number
+            // interp. as the dx value along the x axis
+            let INCREMENT = 0.1
+
+            // listOfDerivativesAndFunctions is [Number, Number]
+            // the first item of the array represents the DERIVATIVE, 
+            // the second item represents F(X).
+            // Each tuple pair represents a new increment of x by INCREMENT 
+            let listOfDerivativesAndFunctions = takeDx(this.latexToExpression(latex), start, end, INCREMENT)
+
+            // interval is Number
+            // interp. as the number of milliseconds per interval of setInterval
+            let interval = 30
+
+            var point = start
+            var index = 0
+            let finish = listOfDerivativesAndFunctions.length
+
+            var visualize = setInterval(() => {
+                if (index >= finish) {
+                    initializeGraph.removeExpression({id: "visualizationGraph"})
+                    clearInterval(visualize)
+                } else {
+                  //  console.log("point:", point)
+                  //  console.log("index:", index)
+                    let linearFunc = listOfDerivativesAndFunctions[index][0] + "(x -" + point + ") +" + listOfDerivativesAndFunctions[index][1]
+                    initializeGraph.setExpression({id:"visualizationGraph",latex:linearFunc, color: "#c74440"})
+                    index++
+                    point += INCREMENT 
+                }
+            }, interval);
     }
 }
 
